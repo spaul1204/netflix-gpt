e2791dc93276d4076b152a256c9bd286a9d15fd1
Vaibhav Singh Rajput <vssmartniceman97@gmail.com>
2024-09-10 17:32:36 +0530

    modified video background

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,11 +8,14 @@ const VideoBackground = ({ movieId }) => {
   const movieTrailer = useSelector((store) => store.movies?.trailer);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${movieTrailer?.key}`}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          movieTrailer?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
